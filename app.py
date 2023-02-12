d53b84a593db9b85b1961e928a768ad75f3cd656
Diana-Barills <Dianabarillas00@gmail.com>
2023-02-13 01:08:28 +0500

    first

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d44eb28e382cde683c066551138b50428faad458e73dc75517a35456f2148b5f
+size 2843
---
+++ static/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e23938fb331bfaac0ea26b85ad480aeb5b1a444e74f8f3d949da00cba6541dd
+size 6222
